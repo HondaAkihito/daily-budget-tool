4bf34a9004f074e0672e79a6b5cd3db00737178e
honaki-days <ice.make.sky.sakura@icloud.com>
2024-04-17 20:06:06 +0900

    “20240417-「design_プロフィールページ」-完了”

--- app/app/Http/Controllers/BudgetController.php
+++ app/app/Http/Controllers/BudgetController.php
@@ -28,7 +28,9 @@ class BudgetController extends Controller
         // 支出詳細
         // return view('original.spending.show');
         // 支出編集
-        return view('original.spending.edit');
+        // return view('original.spending.edit');
+        // プロフィール
+        return view('original.profile.index');
     }
 
     /**
---
+++ app/resources/views/original/profile/index.blade.php
@@ -0,0 +1,32 @@
+@extends('parents.layout')
+@section('content')
+
+<section class="container">
+
+    <h2 class="mt-5 text-center">プロフィール</h2>
+    <div class="profile-picture mt-5 mx-auto">
+        <img class="img-fluid" src="/assets/sample.jpg" alt="Profile Picture">
+    </div>
+    <div class="mb-5 mt-2 text-center">
+        <button type="submit" class="btn btn-success">編集</button>
+    </div>
+
+    <div class="form-group d-flex border-bottom">
+        <label for="name" class="col-form-label w-25">名前</label>
+        <input type="text" class="form-control-plaintext" id="name" value="名前です" disabled readonly>
+    </div>
+    <div class="form-group d-flex border-bottom">
+        <label for="email" class="col-form-label w-25">メールアドレス</label>
+        <input type="text" class="form-control-plaintext" id="email" value="a@a.a" disabled readonly>
+    </div>
+
+    <!-- オールリセット --><!-- ログアウト -->
+    <form>
+        @csrf
+        <button type="submit" class="btn btn-danger mt-4">予算/支出をリセット</button>
+        <button class="btn btn-dark mt-4 ml-3">ログアウト</button>
+    </form>
+
+</section>
+
+@endsection
