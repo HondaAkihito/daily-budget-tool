e578f14f29e205cc44b2be63eb3ea91473aa1383
honaki-days <ice.make.sky.sakura@icloud.com>
2024-04-17 15:00:58 +0900

    “20240417-「MVC_テーブル作成」-完了”

---
+++ app/database/migrations/2024_04_17_055319_create_spendings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSpendingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('spendings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title', '20');
+            $table->integer('amount');
+            $table->date('date');
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('spendings');
+    }
+}
