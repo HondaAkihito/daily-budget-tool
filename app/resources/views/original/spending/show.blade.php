b7a3de29631f7cf73bf935ffd57a81c35dc61dd3
honaki-days <ice.make.sky.sakura@icloud.com>
2024-04-17 19:55:19 +0900

    “20240417-「design_支出詳細ページ」-完了”

---
+++ app/resources/views/original/spending/show.blade.php
@@ -0,0 +1,27 @@
+@extends('parents.layout')
+@section('content')
+
+<section class="container">
+    <h2 class="mt-5 text-center">支出詳細</h2>
+    <form class="mt-5 date_option">
+        @csrf
+        <div class="form-group d-flex border-bottom">
+            <label for="date" class="col-form-label w-25">日付</label>
+            <input type="date" id="date" class="form-control-plaintext" value="2022-08-03" disabled readonly>
+        </div>
+        <div class="form-group d-flex border-bottom">
+            <label for="title" class="col-form-label w-25">タイトル</label>
+            <input type="text" class="form-control-plaintext" id="title" value="aaa" disabled readonly>
+        </div>
+        <div class="form-group d-flex border-bottom">
+            <label for="amount" class="col-form-label w-25">支出金額</label>
+            <input type="number" class="form-control-plaintext" id="amount" value="12345" disabled readonly>
+        </div>
+        <div class="mt-4">
+            <button type="submit" class="btn btn-success">編集</button>
+            <button type="submit" class="btn btn-danger ml-5">削除</button>
+        </div>
+    </form>
+</section>
+
+@endsection
