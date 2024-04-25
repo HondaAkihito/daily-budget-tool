dd220d5b4eeed67c1d682fc7b33b7261c3d60bf2
honaki-days <honaki.and.1114@gmail.com>
2024-04-25 16:23:38 +0900

    “20240425-「function_ポリシークラス」-完了”

--- app/app/Providers/AuthServiceProvider.php
+++ app/app/Providers/AuthServiceProvider.php
@@ -5,6 +5,9 @@ namespace App\Providers;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
+use App\Spending; // 追記
+use App\Policies\SpendingPolicy; // 追記
+
 class AuthServiceProvider extends ServiceProvider
 {
     /**
@@ -14,6 +17,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         // 'App\Model' => 'App\Policies\ModelPolicy',
+        'App\Spending' => 'App\Policies\SpendingPolicy',
     ];
 
     /**
--- app/routes/web.php
+++ app/routes/web.php
@@ -27,10 +27,22 @@ Route::group(['middleware' => 'auth'], function() {
   Route::resource('/', 'TopController');
   // 予算登録
   Route::resource('/budget', 'BudgetController');
-  // 支出登録、支出詳細、支出削除、支出編集
-  Route::resource('/spending', 'SpendingController');
-  // マイページ
-  Route::resource('/profile', 'ProfileController');
+
+  // 支出登録
+  // create、storeは、引数でidを必要としないためUser->とSpending->idの一致を確認するモデルバインディングで弾かれてしまう
+  // そのため「create」「store」のみ外側&先に処理する 
+  // ルートモデルバインディングではexceptをしっかり記述
+  Route::resource('/spending', 'SpendingController', ['only' => ['create', 'store']]);
+
+  // ルートモデルバインディング
+  Route::group(['middleware' => 'can:view,spending'], function() {
+    // 支出登録、支出詳細、支出削除、支出編集
+    Route::resource('/spending', 'SpendingController', ['except' => ['create', 'store']]);
+  });
+
+    // マイページ
+    Route::resource('/profile', 'ProfileController');
+
   // 検索
   Route::resource('/search', 'SearchController');
 });
