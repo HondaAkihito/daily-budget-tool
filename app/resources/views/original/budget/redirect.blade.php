3282a691e70dfb8327b9bb336fafb8b80bf90b5a
HondaAkihito <honaki.and.1114@gmail.com>
2024-05-04 18:32:30 +0900

    “20240504-「design_予算登録リダイレクトページのボタンを修正」-完了”

--- app/resources/views/original/budget/redirect.blade.php
+++ app/resources/views/original/budget/redirect.blade.php
@@ -8,13 +8,13 @@
     @include('parents.status')
 
     <!-- 予算表示 -->
-    <h2 class="mt-5 text-center">
-        予算登録が終わったら<br>
-        「予算 / 支出登録」が開放されます
-    </h2>
-    <section class="navbar">
-        <div class="mb-5 mt-2 text-center w-100">
-            <a href="{{ route('budget.create') }}" class="btn btn-success display">予算登録</a>
+    <div class="mt-5 text-center">
+        「予算登録」が終わったら<br>
+        「予算ボタン」「支出登録ボタン」が開放されます
+    </div>
+    <section class="navbar justify-content-center">
+        <div class="mb-5 mt-2 text-center">
+            <a href="{{ route('budget.create') }}" class="btn btn-success display budget-redirect-a">予算登録</a>
         </div>
     </section>
 </div>
