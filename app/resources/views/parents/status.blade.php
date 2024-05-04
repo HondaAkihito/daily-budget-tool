65dcb6fdab4124d761fac4e24afb547a65f2d523
HondaAkihito <honaki.and.1114@gmail.com>
2024-05-04 18:56:31 +0900

    “20240504-「design_フラッシュメッセージ_赤く装飾」-完了”

--- app/resources/views/parents/status.blade.php
+++ app/resources/views/parents/status.blade.php
@@ -1,3 +1,3 @@
 @if(session()->has('status'))
-    <div class="text-center">{{session('status')}}が完了しました！</div>
+    <div class="text-center border border-danger text-danger">{{session('status')}}が完了しました！</div>
 @endif
